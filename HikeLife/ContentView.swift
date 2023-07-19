314eea16d81c976dda37ce62d55814254947ac5a
Abdullah Bilgin <abilgin02@icloud.com>
2023-07-19 21:07:33 +0300

    Project Setup (Icon, image and color)

---
+++ HikeLife/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HikeLife
+//
+//  Created by Abdullah Bilgin on 7/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HikeLife/HikeLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeLifeApp.swift
+//  HikeLife
+//
+//  Created by Abdullah Bilgin on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
